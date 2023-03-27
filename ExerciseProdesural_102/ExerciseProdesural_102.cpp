5c8ab42200ed71202f99d3289865bf7353bc7598
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-03-27 10:43:45 +0700

    exercise

--- ExerciseProdesural_102/ExerciseProdesural_102.cpp
+++ ExerciseProdesural_102/ExerciseProdesural_102.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+double rerata(double a, double b) {
+    return (a + b) / 2;
+}
+
+string status(double rata) {
+    if (rata >= 70)
+        return "Selamat Anda Diterima!!!";
+    else
+        return "Maaf Anda Gagal";
+}
+string status2(double rata, double nil) {
+    if (rata >= 70 or nil >= 80)
+        return "Selamat Anda Diterima!!!";
+    else
+        return "Maaf Anda Gagal";
+}
+string nama;
+int j;
+
+void input() {
+    for (int i = 0; i < 20; i++) {
+        cout << "Masukkan Nama = ";
+    }
+    return 0;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    input();
 }
